0654a031c37ca0f92db81f5a4cbafb7c076905c5
ExRyze <vaisyagovinandas@gmail.com>
2022-09-12 15:55:24 +0800

    Update - XHR to AJAX fetch

--- Pemula - Menengah/src/index.js
+++ Pemula - Menengah/src/index.js
@@ -15,29 +15,18 @@ class animeCard extends HTMLElement {
 };
 customElements.define("anime-card", animeCard);
 
-const xhr = new XMLHttpRequest();
+async function getAnime() {
+  try {
+    const response = await fetch("https://api.jikan.moe/v4/anime");
+    const allAnime = await response.json();
+    allAnime.data.forEach(anime => {
+      const card = document.createElement("anime-card");
+      card.setAttribute("src", anime.images.jpg.image_url);
+      document.body.querySelector(".card-body").appendChild(card);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
 
-xhr.onload = function () {
-  const allAnime = JSON.parse(this.responseText);
-  
-  allAnime.data.forEach(anime => {
-    const card = document.createElement("anime-card");
-    card.setAttribute("src", anime.images.jpg.image_url);
-    document.body.querySelector(".card-body").appendChild(card);
-  });
-};
- 
-xhr.onerror = function () {console.log('Ups something error');};
-
-xhr.open('GET', 'https://api.jikan.moe/v4/anime');
-xhr.send();
-// xhr.setRequestHeader('Content-Type', 'application/json');
-// xhr.setRequestHeader('X-Auth-Token', '12345');
- 
-// const book = {
-//   id: 10,
-//   title: 'Edensor',
-//   author: 'Andrea Hirata',
-// };
- 
-// xhr.send(JSON.stringify(book));
+getAnime();
