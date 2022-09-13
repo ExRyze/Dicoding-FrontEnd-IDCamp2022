e6475322966978700568fbf01c0de9688cd3e9b3
ExRyze <vaisyagovinandas@gmail.com>
2022-09-13 09:56:57 +0800

    Update - Web Component

--- Pemula - Menengah/src/index.js
+++ Pemula - Menengah/src/index.js
@@ -3,13 +3,20 @@ import "./style/style.css";
 import $ from "jquery";
 
 class animeCard extends HTMLElement {
-  connectedCallback() {
-    this.src = $(this).attr("src") || null;
+  set datas(dataset) {
+    this._dataset = dataset;
+    this.create();
+  }
 
+  create() {
     this.innerHTML = `
-    <div class="card bg-secondary w-100" role="button">
-      <div class="card-body">
-        <img src="${this.src}" class="w-25">
+    <div class="card bg-secondary w-100 text-white" role="button">
+      <div class="card-body d-flex">
+        <img src="${this._dataset.img_src}" class="col-md-3 col-4">
+        <div class="col-md-9 col-8 ps-3 d-flex flex-column">
+          <h3 class="h3">${this._dataset.title}</h3>
+          <h6 class="h6">Score: ${this._dataset.score}<h6>
+        </div>
       </div>
     </div>
     `;
@@ -21,12 +28,21 @@ async function getAnime() {
   try {
     const response = await fetch("https://api.jikan.moe/v4/anime");
     const allAnime = await response.json();
-    allAnime.data.forEach(anime => {
-      const card = document.createElement("anime-card");
-      $(card).attr("src", anime.images.jpg.image_url);
-      $(card).attr("class", "col-md-6 col-sm-12 d-flex p-2")
-      $(".display").append(card);
+    let cards = new Promise(function (resolve) {
+      resolve(
+        allAnime.data.forEach(anime => {
+          const card = document.createElement("anime-card");
+          $(card).attr("class", "col-md-6 col-sm-12 d-flex p-2")
+          card.datas = {
+            img_src: anime.images.jpg.image_url,
+            title: anime.title,
+            score: anime.score
+          };
+          $(".display").append(card);
+        })
+      );
     });
+    await cards;
   } catch (error) {
     console.log(error);
   }
